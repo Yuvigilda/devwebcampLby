bfdac9c175f99eab66ea2da451cffaa0abe99398
Yuvigilda <altamiranoyuvigilda06122001@gmail.com>
2026-03-16 08:34:39 -0600

    cambio en router

--- Router.php
+++ Router.php
@@ -42,7 +42,7 @@ class Router{
 
         $contenido = ob_get_clean(); //limpia el bffer
 
-        $url_actual = $_SERVER['PATH_INFO'] ?? '/';
+        $url_actual = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
 
         if(str_contains($url_actual, '/admin')){
             include_once __DIR__ . '/views/admin-layout.php';
